d348a118b43c1721d3a5332f67d1fdbd25d72e06
unknown <pauloulisses1212@gmail.com>
2025-05-20 18:34:40 -0300

    tipos primitivos

---
+++ tipos-primitivos/script.js
@@ -0,0 +1,12 @@
+// Tipos Primitivos
+
+const nome = 'Paulo'; // string
+const nome1 = "Paulo"; // string
+const nome2 = `Paulo`; // string - tamplate
+
+const num1 = 10; // number
+const num2 = 10.52 // number
+
+let nomeAluno; // undefined = não aponta para local na memória
+let sobreNomeAluno = null; // nulo -> não aponta para local na memória
+
