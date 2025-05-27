855a43bb2b8eace2619e4f1131e694d11caed649
paulo <pauloulisses1212@gmail.com>
2025-05-27 18:43:52 -0300

    valores-primitivos

---
+++ valores-primitivos-referencia/script.js
@@ -0,0 +1,25 @@
+/**
+ * Primitivos(imutáveis) -  string, number, boolean, undefined, null (bigint, symbol)
+ * 
+ * 
+ */
+
+// atribuindo um novo valor a variável
+let nome = 'Paulo';
+nome = 'Ulisses';
+console.log(nome);
+
+// copiando o memso valor a duas variáveis
+let a = 'A';
+let b = a;
+console.log(a, b);
+
+
+// Referência (mutável) - array, object, function
+
+// array
+let c = [1, 2, 3];
+let d = a;
+console.log(c, d);
+
+// object {}
